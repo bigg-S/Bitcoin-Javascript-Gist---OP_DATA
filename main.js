ef7f918dcffd262e68522b587f6e6c261d00622b
flakesbig <oketchs702@gmail.com>
2024-01-25 03:20:43 +0300

    initial commit

---
+++ main.js
@@ -0,0 +1,7 @@
+// main.js
+
+// import necessary libraries
+import * as bip39 from '@scure/bip39';
+import { wordlist } from '@scure/bip39/wordlists/english';
+import { HDKey } from '@scure/bip32';
+import * as btc from '@scure/btc-signer';
