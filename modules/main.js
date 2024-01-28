f672cfac4e00114177d2b34a55edf99c63a1f234
flakesbig <oketchs702@gmail.com>
2024-01-28 05:27:02 +0300

    transactions

--- modules/main.js
+++ modules/main.js
@@ -16,21 +16,20 @@ function processInputs() {
     if(fileInput.files.length > 0) {
         const file = fileInput.files[0];
         console.log("getting ready...");
-        helper.readFileAsByteArray(file, (error, byteArray) => {
+        helper.readFileAsBinaryString(file, (error, binaryString) => {
             if(error) {
                 resultDiv.innerHTML = `<span style="color: red;">Error reading file: ${error.message}</span>`;
             } else {                
-                resultDiv.innerHTML = `<p style="color: black;"> File Content (as byte array): ${byteArray.join(',')}</p>`;
-                helper.uploadToBitcoin(byteArray, password);
+                resultDiv.innerHTML = `<p style="color: black;"> File Content (binary string): ${binaryString}</p>`;
+                helper.uploadData(binaryString, password);
             }
         });
     }
 
     // handle text input
     const plainText = textInput.value.trim();
-    if(plainText !== '') {
-        const textByteArray = helper.textToUTF8Array(plainText);        
-        resultDiv.innerHTML += `<p style="color: black;"> Text Content (as utf-8 array): ${textByteArray.join(',')}</p>`;
+    if(plainText !== '') {        
+        resultDiv.innerHTML = resultDiv.innerHTML + `<p style="color: black;"> Text Content (as utf-8 array): ${plainText}</p>`;
     }
 }
 
